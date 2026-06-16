44923154856ae98cad623751968b33dc7919977e
Lukas Randecker <randeckerlukas@gmail.com>
2026-06-16 10:56:40 +0200

    add base change for repo

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/[Responsive-Design---Audi-x-Schachbrett]/",
   plugins: [react(), tailwindcss()],
 })
