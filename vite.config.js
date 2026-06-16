10b7aaa51858d0b19f2d18cb9cde573677bac362
Lukas Randecker <randeckerlukas@gmail.com>
2026-06-16 10:58:26 +0200

    change base name

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/[Responsive-Design---Audi-x-Schachbrett]/",
+  base: "/Responsive-Design---Audi-x-Schachbrett/",
   plugins: [react(), tailwindcss()],
 })
